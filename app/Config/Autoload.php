cc611edd3bdc2990bcdab65b95d57761d5252b6a
David Cardenas Lorca <cine.dcl@gmail.com>
2026-05-07 01:18:53 -0400

    feat(system): add public /health endpoint for liveness probes

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -50,7 +50,8 @@ class Autoload extends AutoloadConfig
         'App\Modules\ApiKeys'   => APPPATH . 'Modules/ApiKeys',
         'App\Modules\Metrics'   => APPPATH . 'Modules/Metrics',
         'App\Modules\Language'  => APPPATH . 'Modules/Language',
-        'App\Modules\Iam'  => APPPATH . 'Modules/Iam',
+        'App\Modules\Iam'       => APPPATH . 'Modules/Iam',
+        'App\Modules\System'    => APPPATH . 'Modules/System',
     ];
 
     /**
